3aaaac0985889f4c8b0b5c10b0588661447ccf52
Razor-tech-256 <razortechcompanyltd@gmail.com>
2024-10-12 12:52:23 +0300

    all added

---
+++ admin/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/login.css">
+</head>
+<body>
+    
+        <center>
+        <div class="container">
+            <fieldset>
+                <legend><img src="images/rentals.png" width="100px" height="100px" alt=""></legend>
+       
+        <form action="" method="post">
+            <input type="email" name="email" id="">
+            <input type="password" name="password" id="">
+            <input type="submit" value="login">
+        </form>
+        </fieldset>
+</center>
+    </div>
+</body>
+</html>                                     
